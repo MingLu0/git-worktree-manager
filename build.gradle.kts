b3f7090ca340db54c4311bc28bb5c5600043a7c6
Ming Luo <luoming886@gmail.com>
2026-06-09 18:18:27 +1200

    chore: bump version to 1.1.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.purringlabs.gitworktree"
-version = "1.1.16"
+version = "1.1.18"
 
 repositories {
     mavenCentral()
@@ -44,7 +44,7 @@ intellijPlatform {
         changeNotes = """
             <b>What's new</b>
             <ul>
-              <li>sanitize branch names entered in create-worktree flow</li>
+              <li>Prepare release 1.1.18</li>
             </ul>
 """.trimIndent()
     }
